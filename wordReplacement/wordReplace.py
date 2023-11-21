d623ea3ecc6866f0584ad8fd9c6338cc97fcfc6e
BigT001 <unclet1992@gmail.com>
2023-11-20 16:20:37 -0800

    word_replace

---
+++ wordReplacement/wordReplace.py
@@ -0,0 +1,10 @@
+def replace_word():
+
+    My_str = "This is a program that replaces word with another"
+
+    word_to_replace = input("Enter word to replace: ")
+    new_word = input("Enter new word: ")
+
+    print(My_str.replace(word_to_replace, new_word))
+
+replace_word()
