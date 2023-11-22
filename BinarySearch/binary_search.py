539219f089aedcdc73dc18ae26bc0e886c7e1d49
BigT001 <unclet1992@gmail.com>
2023-11-22 01:02:35 -0800

    Split emial

--- BinarySearch/binary_search.py
+++ BinarySearch/binary_search.py
@@ -1,28 +1,31 @@
-def binary_search(list, target):
+def binary_search(lst, target):
     middle = 0
     start = 0
-    end = len(list)
+    end = len(lst)
     steps = 0
 
-    while(start<=end):
-        print("step", steps, ":", str(list[start:end+1]))
+    while start <= end:
+        print(f"Step {steps}: {lst[start:end+1]}")
 
-        steps = steps + 1
+        steps += 1
         middle = (start + end) // 2
 
-        if target == list[middle]:
+        if target == lst[middle]:
             return middle
         
-        if target < list[middle]:
-            end = middle -1
-
+        if target < lst[middle]:
+            end = middle - 1
         else:
             start = middle + 1
 
     return -1
 
-Mylist = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-tarGet = 5
+my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
+target_value = 20
 
-result = binary_search(Mylist, tarGet)
-print(f"Target {tarGet} found at index {result}" if result != -1 else f"Target {tarGet} not found in the list")
+result = binary_search(my_list, target_value)
+
+if result != -1:
+    print(f"Target {target_value} found at index {result}")
+else:
+    print(f"Invalid index. Target {target_value} not found in list.")
--- split_email/split.py
+++ split_email/split.py
@@ -3,9 +3,11 @@
 
 def split():
     print("Welcome to email spliter!" "\n")
-   # print("")
 
-    input_email = input("Enter your email address: ")
+    input_email = input("Enter your email address (or type 'Exit' to end): ")
+
+    if input_email.lower() == 'exit':
+        return True
 
     components = input_email.split("@")
 
@@ -17,9 +19,22 @@ def split():
         print("domain: ", domain)
         print("extension: ", extension)
         print("")
+        return False
 
     else:
-        print("Invalid email address format. please try again.")
+        print("Invalid email address format. please try again or type 'exit' to end.")
+        return False
 
 while True:
-     split()
+     should_exit = split()
+     if should_exit:
+         print("Exiting splitter...")
+         break
+
+     user_input = input("Do you want to split another email? (yes/no): ")
+     if user_input.lower() != "yes":
+         confirm_exit = input("Are you sure you want to exit spliter?...(yes/no)")
+
+         if confirm_exit.lower() == "yes":
+            print("Exiting splitter...")
+            break
