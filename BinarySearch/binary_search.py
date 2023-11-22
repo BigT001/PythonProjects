c00213b446f87692e6da42da81c27d6576848995
BigT001 <unclet1992@gmail.com>
2023-11-22 01:59:24 -0800

    Binary search updated

--- BinarySearch/binary_search.py
+++ BinarySearch/binary_search.py
@@ -1,7 +1,7 @@
 def binary_search(lst, target):
     middle = 0
     start = 0
-    end = len(lst)
+    end = len(lst) - 1  # Adjusted end to prevent index out of range
     steps = 0
 
     while start <= end:
@@ -20,12 +20,31 @@ def binary_search(lst, target):
 
     return -1
 
-my_list = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20]
-target_value = 20
+def check_target_range(target, lst):
+    if not lst:
+        return False
 
-result = binary_search(my_list, target_value)
+    min_value, max_value = lst[0], lst[-1]
+    
+    if not (min_value <= target <= max_value):
+        print(f"Error: Target {target} is out of range [{min_value}, {max_value}].")
+        return False
 
-if result != -1:
-    print(f"Target {target_value} found at index {result}")
-else:
-    print(f"Invalid index. Target {target_value} not found in list.")
+    return True
+
+genNum = list(range(1, 201))
+
+while True:
+    try:
+        target_value = int(input("Enter Target: "))
+    except ValueError:
+        print("Invalid input. Please enter a valid number.")
+        continue
+
+    if check_target_range(target_value, genNum):
+        result = binary_search(genNum, target_value)
+
+        if result != -1:
+            print(f"Target {target_value} found at index {result}")
+        else:
+            print(f"Target {target_value} not found in list.")
--- wordReplacement/README.md
+++ wordReplacement/README.md
@@ -3,13 +3,13 @@ This program performs the following tasks:
 Input String:
 
 Initializes a string variable my_str with the value "With this you can replace words with another".
-User Input:
 
+User Input:
 Prompts the user to input a word to be replaced (word_to_replace).
 Prompts the user to input a new word that will replace the specified word (new_word).
+
 Replacement:
-
 Uses the replace() method on the string (my_str) to replace occurrences of the specified word (word_to_replace) with the new word (new_word).
-Output:
 
+Output:
 Prints the modified string with the replacements.
